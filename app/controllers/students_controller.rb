136f8a393a102014d680b5d46ba7dccff08bd590
Joseph <joffan98@gmail.com>
2022-11-23 11:09:10 -0600

    commit tree d2ca5c45e738df56363f1e962e1a90b3f9055005

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,22 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    students = params[:name] ? Student.by_name(params[:name]) : Student.all
     render json: students
   end
 
+  def highest_grade
+    student = Student.all.order(grade: :desc).first
+    render json: student
+  end
+
+  def grades
+    students = Student.all.order(grade: :desc)
+    render json: students
+  end
+  
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -4,4 +4,9 @@ class Student < ApplicationRecord
     "#{first_name} #{last_name}"
   end
 
+  def self.by_name(name)
+    first_name = Student.find_by(first_name: name.capitalize)
+    student = first_name ? first_name : Student.find_by(last_name: name.capitalize)
+  end
+
 end
